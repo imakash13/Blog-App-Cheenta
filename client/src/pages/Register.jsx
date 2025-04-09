b0a5c64fd9a87b2eebf060378f3831d295cae755
Akash Srivastava <118252963+imakash13@users.noreply.github.com>
2025-04-09 19:32:31 +0530

    Update Register.jsx

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -9,7 +9,7 @@ export default function Register() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post('http://localhost:5000/api/auth/register', form);
+      await axios.post('https://render.com/docs/web-services#port-binding/api/auth/register', form);
       alert('Registered successfully');
       navigate('/login')
     } catch (err) {
