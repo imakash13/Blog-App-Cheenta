cc9447f3128dcb51359b914775e29199c56c0aab
Akash Srivastava <118252963+imakash13@users.noreply.github.com>
2025-04-09 19:51:09 +0530

    Update Register.jsx

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -9,7 +9,7 @@ export default function Register() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post('https://render.com/docs/web-services#port-binding/api/auth/register', form);
+      await axios.post('https://blog-app-uke9.onrender.com/api/auth/register', form);
       alert('Registered successfully');
       navigate('/login')
     } catch (err) {
